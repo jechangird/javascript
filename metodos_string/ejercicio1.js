c564381fb5e64aeb1dd1a6b067a6fbe351a1a552
jechangird <changir@hotmail.com>
2025-07-08 20:35:07 -0400

    Create ejercicio1.js

---
+++ metodos_string/ejercicio1.js
@@ -0,0 +1,54 @@
+/*
+  Metodos de strings
+  Los métodos de strings son funciones que se pueden aplicar a cadenas de texto para realizar diversas operaciones.
+  Algunos de los métodos más comunes son:
+  - `toUpperCase()`: Convierte la cadena a mayúsculas.
+  - `toLowerCase()`: Convierte la cadena a minúsculas.
+  - `substring()`: Extrae una parte de la cadena.
+  - `indexOf()`: Busca la posición de un carácter o subcadena en la cadena
+  - `replace()`: Reemplaza una parte de la cadena por otra.
+  - `split()`: Divide la cadena en un array de subcadenas.
+  - `trim()`: Elimina los espacios en blanco al inicio y al final de la cadena.
+  - `includes()`: Verifica si una subcadena está presente en la cadena.
+  - `charAt()`: Devuelve el carácter en una posición específica.
+  - `length`: Propiedad que devuelve la longitud de la cadena.
+  - `startsWith()`: Verifica si la cadena comienza con una subcadena específica.
+  - `endsWith()`: Verifica si la cadena termina con una subcadena específica.
+  - `repeat()`: Repite la cadena un número específico de veces.
+  - `concat()`: Une dos o más cadenas.
+*/
+
+
+// Crea una función que reciba un string pero retorne cuantas palabras tiene.
+
+function contar_palabras(texto){
+  return texto.trim().split(" ").length;
+}
+
+console.log(contar_palabras("     hola a todos en la clase !  "));
+
+//2. Crea una función que reciba un string y cuente cuantas vocales tiene
+function contar_vocales(texto) {
+  contador= 0;
+  for (let letra of texto.toLowerCase()){
+    if ("aeiouAEIOU".includes(letra)){
+      contador++;
+    }
+  }
+  return contador;
+})
+
+console.log(contar_vocales("este es una prueba de contar vocales"));
+
+ //3. Crea una función que reciba un string y retorne el string al revés.
+ function invertir_palabra(texto){
+  let palabraInvertida = "";
+  for(let i = texto.lenght -1; i >= 0; i-- ){
+    palabraInvertida += texto[i];
+  }
+  return palabraInvertida;
+ }
+
+ console.log(invertir_texto("yo soy tu padre"))
+
+
