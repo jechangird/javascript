d45b365538d2e1192223380a06ce7a039ac4a463
jechangird <changir@hotmail.com>
2025-07-10 15:59:00 -0400

    Delete factorial.js

--- factorial/factorial.js
+++
@@ -1,9 +0,0 @@
-function factorial(n) {
-  if (n < 0) return "El factorial no existe para números negativos";
-  if (n === 0 || n === 1) return 1;
-  
-  return n * factorial(n - 1);
-}
-
-console.log(factorial(6)); 
-
