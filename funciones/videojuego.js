0687768e280a2eaff5cef9b007d7f51eaf7af15e
jechangird <changir@hotmail.com>
2025-07-15 15:57:12 -0400

    15/07/2025

---
+++ funciones/videojuego.js
@@ -0,0 +1,26 @@
+//clase personajes
+
+class Personajes {
+    constructor(nombre, vida, dano, defensa, velocidad){
+        this.nombre = nombre;
+        this.vida = vida;
+        this.dano = dano;
+        this.defensa = defensa;
+        this.velocidad = velocidad;
+    }
+    //metodo atacar
+    atacar(){
+        console.log(´${this.nombre} esta atacando´);
+
+
+    }
+
+    //metodo saludar
+    saludar(){
+         console.log(´$ Hola, soy{this.nombre}´);
+    }
+}
+
+class Guerrero extends Personajes {
+    
+}
