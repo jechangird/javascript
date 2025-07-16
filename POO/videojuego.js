96bbbd4b8297707bf9e0cd212b000445bddf8c68
jechangird <changir@hotmail.com>
2025-07-16 15:02:54 -0400

    16/07/2025

--- POO/videojuego.js
+++ POO/videojuego.js
@@ -1,12 +1,13 @@
 //clase personajes
 
 class Personajes {
-    constructor(nombre, vida, dano, defensa, velocidad){
+    constructor(nombre, vida, dano, defensa, velocidad,clase){
         this.nombre = nombre;
         this.vida = vida;
         this.dano = dano;
         this.defensa = defensa;
         this.velocidad = velocidad;
+        this.clase = clase;
     }
     //metodo atacar
     atacar(){
@@ -17,12 +18,12 @@ class Personajes {
 
     //metodo saludar
     saludar(){
-        console.log(`Hola, soy ${this.nombre}`);
+        console.log(`Hola, soy ${this.nombre} y soy ${this.clase}`);
     }
 }
 
 class Guerrero extends Personajes {
-    super(nombre, vida, dano, defensa, velocidad){
+    super(nombre, vida, dano, defensa, velocidad, clase){
 
     }
     //metodo atacar con armas
@@ -36,7 +37,7 @@ class Guerrero extends Personajes {
 
 //class mago
 class Mago extends Personajes {
-    super(nombre, vida, dano, defensa, velocidad){
+    super(nombre, vida, dano, defensa, velocidad,clase){
 
     }
     //metodo atacar con armas
@@ -48,15 +49,29 @@ class Mago extends Personajes {
     }
 }
 
+//class arquero
+class Arquero extends Personajes {
+    super(nombre, vida, dano, defensa, velocidad, clase){
 
+    }
+    //metodo disparar
+    disparar(){
+        let flechas = ["flecha plana", "flecha gorda", "flechita", "flecha envenenada"];
+        let flecha = flechas[2];
+        console.log(`${this.nombre} esta atacando con ${flecha}`);
 
+    }
+}
 
-//let armas = ["liguita", "palito", "cortaunas", "pistola de agua"];
-//let arma = armas[1];
 let jose = new Guerrero("jose", 5, 3, 8, 100);
 jose.saludar();
 jose.atacar_con_armas();
 
 let glafira = new Mago("glafira", 5, 3, 8, 100);
+glafira.saludar()
 glafira.atacar_con_hechizos();
+
+let toro = new Arquero("toro", 5, 3, 8, 100);
+toro.saludar()
+toro.disparar();
 
