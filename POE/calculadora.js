6e2b026d9b2a6725bc0cd20e45623b0b7a82d2d5
jechangird <changir@hotmail.com>
2025-07-28 16:01:49 -0400

    28/07/2025

--- POE/calculadora.js
+++ POE/calculadora.js
@@ -1,10 +1,8 @@
 const nroUno = document.querySelector("#numeroUno");
 const nroDos = document.querySelector("#numeroDos");
 const resultado = document.querySelector("#resultado");
-const btnSuma = document.querySelector("#suma");
-const btnResta = document.querySelector("#resta");
-const btnMultiplicacion = document.querySelector("#multiplicacion");
-const btnDivision = document.querySelector("#division");
+const botones = document.querySelectorAll(".btn");
+
 const btnTotal = document.querySelector("#total");
 const btnLimpiar = document.querySelector("#limpiar");
 
@@ -17,4 +15,30 @@ btnLimpiar.addEventListener("click", () => {
   resultado.value = 0.00;
   
 });
+
+
+botones.forEach(boton => {
+    boton.addEventListener("click", () => {
+      const n1 = parseFloat(nroUno.value);
+      const n2 = parseFloat(nroDos.value);
+      const operacion = boton.textContent;
+      let total;
+
+      if (isNaN(n1) || isNaN(n2)) {
+        resultado.textContent = "⚠️ Ingrese ambos números.";
+        return;
+      }
+
+      switch (operacion) {
+        case "+": total = n1 + n2; break;
+        case "-": total = n1 - n2; break;
+        case "*": total = n1 * n2; break;
+        case "/": 
+          total = n2 !== 0 ? n1 / n2 : "❌ Error: división por cero"; 
+          break;
+      }
+
+      resultado.textContent = `Resultado: ${total}`;
+    });
+  });
 
